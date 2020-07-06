554e9ecc586d20dd45c187d77c0ed28c7833eec6
github username <github email address>
2020-07-06 15:05:42 -0400

    ..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :user_owned_guns
     has_many :guns, through: :user_owned_guns
+    #has_secure_password
+    #^ this will force all users to have a password, makes it so we dont need validates :password
+    #validates :username, uniqueness: { case_sensitive: false}
 end
