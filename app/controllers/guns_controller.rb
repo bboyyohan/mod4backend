73a7f9796671c9aa7d151515af43bf76eb0cea74
github username <github email address>
2020-07-07 15:45:23 -0400

    controller for user_owned_guns

--- app/controllers/guns_controller.rb
+++ app/controllers/guns_controller.rb
@@ -3,4 +3,6 @@ class GunsController < ApplicationController
     def index
         render json: Gun.all
     end
+
+    # def destroy
 end
--- app/controllers/user_owned_guns_controller.rb
+++ app/controllers/user_owned_guns_controller.rb
@@ -1,11 +1,16 @@
 class UserOwnedGunsController < ApplicationController
 
+    def index
+        render json: UserOwnedGun.all
+    end 
+    
     def show
         # byebug
-        user_owned_gun = UserOwnedGun.where(user_id: params[:id])
+        user_owned_gun = UserOwnedGun.find(params[:id])
         render json: user_owned_gun
     end
 
+    
     # def getUserGuns
     #     user = User.all.find(params[:id])
     #     render json: user.user_owned_guns.to_json(:include => {
@@ -14,4 +19,8 @@ class UserOwnedGunsController < ApplicationController
     #         }
     #     })
     # end 
+    def destroy
+        UserOwnedGun.destroy(params[:id])
+    
+    end 
 end
